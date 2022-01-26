57785e0a235eeda610feeb36310571904c0b7234
ktoan <nktoanwork@gmail.com>
2022-01-26 11:23:52 +0700

    third

--- rest-api/routes/auth.js
+++ rest-api/routes/auth.js
@@ -1,14 +1,11 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-
 // Register
-
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
-    const newPassword = bcrypt.hash(req.body.password, salt);
-    console.log(req.body.password);
+    const newPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -20,5 +17,19 @@ router.post("/register", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(404).json("User not found!");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(401).json("Wrong password!");
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 module.exports = router;
--- rest-api/routes/user.js
+++ rest-api/routes/user.js
@@ -1,7 +1,23 @@
+const User = require("../models/User");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("user test");
+router.get("/users", (req, res) => {
+  try {
+    const users = User.find()
+      .then((data) => {
+        if (!data) {
+          res.status(404).json("Not found!");
+        } else {
+          res.status(200).json(data);
+        }
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
